5492028d4280aa306c6946e1d1cb302227151b6d
dieydiey <152610220+dieydiey@users.noreply.github.com>
2025-12-18 22:38:32 +0000

    Delete brasil_burger directory

--- brasil_burger/src/main/java/sn/ism/brasilburger/exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package sn.ism.brasilburger.exceptions;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
