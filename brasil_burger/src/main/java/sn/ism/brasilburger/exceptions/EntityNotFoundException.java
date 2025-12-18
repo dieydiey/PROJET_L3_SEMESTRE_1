b6f9e480370ee4b880496d79ac9f9dbb011634a8
dieydiey <152610220+dieydiey@users.noreply.github.com>
2025-12-18 23:29:39 +0000

    Delete brasil_burger directory

--- brasil_burger/src/main/java/sn/ism/brasilburger/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package sn.ism.brasilburger.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
