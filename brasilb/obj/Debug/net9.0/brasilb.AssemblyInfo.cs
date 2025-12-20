2afb2403ba94d73555fb7e4752b547f832474746
Dieynaba <dieynabab8@gmail.com>
2025-12-20 23:05:53 +0000

    vue des produits du nier

--- brasilb/obj/Debug/net9.0/brasilb.AssemblyInfo.cs
+++ brasilb/obj/Debug/net9.0/brasilb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("brasilb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88656b08a0e625af56035afdc49eb8ef22ae8ed2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e5f54bbcc2177ea14cb1940f8eb86a5d47c312")]
 [assembly: System.Reflection.AssemblyProductAttribute("brasilb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("brasilb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
